032f69729c03b7d5913ce3c15ab48c462bb37e92
Full-Daniel-Stack <50594387+Full-Daniel-Stack@users.noreply.github.com>
2023-02-16 12:53:59 -0300

    3 files modified

--- index.ts
+++ index.ts
@@ -1,44 +1,58 @@
 // Import stylesheets
 import './style.css';
 
-let exercicio: number = 0;
-let tempo: number = 0;
-let ciclo = 8;
+let pausa: number = 0;
+let tempo: number = 1;
+let ciclo = 1;
 let relogio = 0
+let time = -1;
 let cronometro = document.getElementById("cronometro");
 let ciclos =  document.getElementById("ciclo")
-
+let body = document.getElementById('tabata');
+let botao = document.getElementById('start')
 function cronometro_exercicio(){
 
-  ciclos.innerHTML = `ciclo ${ciclo}`
+  
+  ciclos.innerHTML = `Ciclo 0${ciclo}`
 
+  // cuida do cronometro
   relogio = setInterval(() =>{
     cronometro.innerHTML = `Treine ${tempo}`
-    tempo++
+    body.style.backgroundColor = 'red';
+    body.style.color = 'white';
+    tempo++ 
 
-    if(tempo > 19){
-      cronometro.innerHTML = `Descanse ${tempo}`
-      ciclos.innerHTML = `ciclo ${ciclo}`
-      
-        if(tempo == 30){
-            tempo = 0;
-            cronometro.innerHTML = `Timer ${tempo}`
-            ciclo = ciclo -1
-            ciclos.innerHTML = `ciclo ${ciclo}`
-            if(ciclo = 0){
-              alert("Treino concluido");
-            }
-          }
+    if(tempo >= 21){
+      body.style.backgroundColor = 'green';
+      cronometro.innerHTML = `Descanse ${tempo-1}`
+    }
+    if(tempo == 30){
+      tempo = 0;
     }
   }, 1000)
 
-  
+
+
+
+  // cuida somente dos ciclos
+  pausa = setInterval(() =>{    
+      time++;
+      ciclo = ciclo - 1;
+      ciclos.innerHTML = `Ciclo 0${ciclo}`;  
+      console.log(ciclo)
+
+      if(ciclo < 1){
+        window.location.reload();
+      }
+
+  },30000)
     
   
 }
 
 
 
-document.getElementById('start').addEventListener('click', (() =>{
-  cronometro_exercicio()
+botao.addEventListener('click', (() =>{
+  cronometro_exercicio();
+  (document.getElementById('start') as HTMLInputElement).disabled = true;
 }))
