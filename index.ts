dc3ffe0f8385fda912d32c555c1789b1ac5c54a1
Full-Daniel-Stack <50594387+Full-Daniel-Stack@users.noreply.github.com>
2023-02-15 22:01:14 -0300

    falta reparos

--- index.ts
+++ index.ts
@@ -1,6 +1,44 @@
 // Import stylesheets
 import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let exercicio: number = 0;
+let tempo: number = 0;
+let ciclo = 8;
+let relogio = 0
+let cronometro = document.getElementById("cronometro");
+let ciclos =  document.getElementById("ciclo")
+
+function cronometro_exercicio(){
+
+  ciclos.innerHTML = `ciclo ${ciclo}`
+
+  relogio = setInterval(() =>{
+    cronometro.innerHTML = `Treine ${tempo}`
+    tempo++
+
+    if(tempo > 19){
+      cronometro.innerHTML = `Descanse ${tempo}`
+      ciclos.innerHTML = `ciclo ${ciclo}`
+      
+        if(tempo == 30){
+            tempo = 0;
+            cronometro.innerHTML = `Timer ${tempo}`
+            ciclo = ciclo -1
+            ciclos.innerHTML = `ciclo ${ciclo}`
+            if(ciclo = 0){
+              alert("Treino concluido");
+            }
+          }
+    }
+  }, 1000)
+
+  
+    
+  
+}
+
+
+
+document.getElementById('start').addEventListener('click', (() =>{
+  cronometro_exercicio()
+}))
