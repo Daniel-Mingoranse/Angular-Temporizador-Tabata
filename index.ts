149fac5fee052898c9d315cbfa7c2dc4aa6aaa26
Full-Daniel-Stack <50594387+Full-Daniel-Stack@users.noreply.github.com>
2023-02-28 15:17:11 -0300

    3 files modified

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ let relogio = 0
 let time = 0;
 let cronometro = document.getElementById("cronometro");
 let ciclos =  document.getElementById("ciclo")
-let body = document.getElementById('tabata');
+let app = document.getElementById('app');
 let botao = document.getElementById('start')
 function cronometro_exercicio(){
 
@@ -18,14 +18,16 @@ function cronometro_exercicio(){
   // cuida do cronometro
   relogio = setInterval(() =>{
     cronometro.innerHTML = `Treine ${tempo}`
-    body.style.backgroundColor = 'red';
-    body.style.color = 'white';
+    app.style.backgroundColor = 'rgb(0, 90, 194)';
+    app.style.color = 'white';
+    botao.style.backgroundColor = 'white'
+    botao.style.color = 'black'
+    botao.innerHTML = 'Aguarde...'
     tempo++ 
 
     if(tempo >= 21){
-      body.style.backgroundColor = 'green';
-      // cronometro.innerHTML = `Descanse ${tempo-1}`
-      // ciclos.innerHTML = `Ciclo 0${ciclo -1}`;  
+      app.style.backgroundColor = 'green';
+      cronometro.innerHTML = `Descanse ${tempo}`
     }
     if(tempo == 31){
       tempo = 1;
@@ -55,5 +57,6 @@ function cronometro_exercicio(){
 
 botao.addEventListener('click', (() =>{
   cronometro_exercicio();
+  alert('Aperte no "Ok" para iniciar seu treino!');
   (document.getElementById('start') as HTMLInputElement).disabled = true;
 }))
